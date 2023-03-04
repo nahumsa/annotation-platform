2104dcae5965bc0f690ca80eb0cc768c4455f850
Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>
2023-03-04 21:38:24 +0000

    add tags to ingestion_form

--- ingestion-api/src/routers/form.py
+++ ingestion-api/src/routers/form.py
@@ -5,15 +5,15 @@ from fastapi.templating import Jinja2Templates
 router = APIRouter()
 templates = Jinja2Templates(directory="templates")
 
-@router.get("/", response_class=HTMLResponse)
+@router.get("/", tags=["ingestion"], response_class=HTMLResponse)
 async def show_form(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
 
-@router.post("/submit/")
+@router.post("/submit/", tags=["ingestion"])
 async def submit_text_entry(text_entry: str = Form(...)):
     # TODO: add data to the argilla server.
     return RedirectResponse(url="/results", status_code=303)
 
-@router.get("/results", response_class=HTMLResponse)
+@router.get("/results", tags=["ingestion"], response_class=HTMLResponse)
 async def results(request: Request):
     return templates.TemplateResponse("result.html", {"request": request})
