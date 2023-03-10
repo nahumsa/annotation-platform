c956d793141c7caa8280c0fe263ece4c223778fa
Nahum Sá <nahumsaa@gmail.com>
2023-03-09 23:24:41 -0300

    add docs

--- ingestion-api/src/routers/form.py
+++ ingestion-api/src/routers/form.py
@@ -7,16 +7,40 @@ templates = Jinja2Templates(directory="src/templates")
 
 
 @router.get("/", tags=["ingestion"], response_class=HTMLResponse)
-async def show_form(request: Request):
+async def show_form(request: Request) -> HTMLResponse:
+    """Shows a form that could be filled to be added to the annotation service.
+
+    Args:
+        request (Request): request received.
+
+    Returns:
+        HTMLResponse: webpage.
+    """
     return templates.TemplateResponse("form.html", {"request": request})
 
 
 @router.post("/submit/", tags=["ingestion"])
-async def submit_text_entry(text_entry: str = Form(...)):
+async def submit_text_entry(text_entry: str = Form(...)) -> RedirectResponse:
+    """Submitts the text entry to the annotation service.
+
+    Args:
+        text_entry (str, optional): text entry from the form that is in "/". Defaults to Form(...).
+
+    Returns:
+        RedirectResponse: Redirects to /results.
+    """
     # TODO: add data to the argilla server.
     return RedirectResponse(url="/results", status_code=status.HTTP_303_SEE_OTHER)
 
 
 @router.get("/results", tags=["ingestion"], response_class=HTMLResponse)
-async def results(request: Request):
+async def results(request: Request) -> HTMLResponse:
+    """Shows a results page.
+
+    Args:
+        request (Request): request received.
+
+    Returns:
+        HTMLResponse: results webpage.
+    """
     return templates.TemplateResponse("result.html", {"request": request})
